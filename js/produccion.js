3bc43710863d0070428af7927883d95dc5522737
Miguel Rueda <miguel.rueda@outlook.com>
2013-04-28 00:53:29 -0500

    Revert "icono de carga"
    This reverts commit 670b9e7840131a955542c879516a1525e6e2fc2c.

--- js/produccion.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-setColors(document, "#FFFF66", "#FFDE00","#FFDE00");
-
-function rePaint(){
-	//setColors(document, "#FFFF00", "#FFd700","#ffd700");
-	setColors(document, "#FFFF66", "#FFDE00","#FFDE00");
-}
