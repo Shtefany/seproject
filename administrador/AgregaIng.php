e9c756452170a2a79fdeb7f572d9fbdc9725f85e
Josué Fernando Ramos <josuehsm@hotmail.com>
2013-04-29 18:25:53 -0500

    Corrección de la opción agregar otro ingrediente
    Cuando se asigna  una receta y se desea incluir una materia prima no
    existente, se selecciona la opción de nuevo ingrediente y agrega uno
    nuevo.

---
+++ administrador/AgregaIng.php
@@ -0,0 +1,29 @@
+<?php 
+	
+	include("../php/Validations.class.php");
+	include("../php/Ingrediente.class.php");	
+	$nombre    =	Validations::cleanString($_GET['nombre']);
+	$area      =	Validations::cleanString($_GET['unidad']);
+
+	
+	if ( Validations::validaNombre($nombre)){
+		if ( !isset($_GET["edit"]) ){
+			$accept     =	Ingrediente::Agregar($nombre,$area);	
+		}
+		else
+		{
+			$accept     =	Ingrediente::Modificar($nombre,$area);	
+		}
+		if(!$accept)
+		{
+			echo "DATABASE_PROBLEM";
+		}
+		else
+		{
+			echo "OK";
+		}		
+	}else{
+		echo "INPUT_PROBLEM";
+	}
+
+?>
